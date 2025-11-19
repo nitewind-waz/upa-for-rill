24d5e0e55cd08de3626139234da95ae16ed39b0f
hilmimahdani <hilmimahdanib@gmail.com>
2025-11-19 13:29:36 +0700

    Add fitur jadwal EPT

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ use App\Http\Controllers\ProdiController;
 use App\Http\Controllers\KelasController;
 use App\Http\Controllers\EptResultMahasiswaController;
 
+use App\Http\Controllers\EptScheduleController;
+
 Route::get('/lololol', function () {
     return Inertia::render('Welcome', [
         'canRegister' => Features::enabled(Features::registration()),
@@ -28,6 +30,23 @@ Route::get('/hasil', [EptResultMahasiswaController::class, 'index'])->name('hasi
 Route::post('/hasil/check', [EptResultMahasiswaController::class, 'checkResult'])->name('hasil.check');
 Route::post('/hasil/stats', [EptResultMahasiswaController::class, 'getStats'])->name('hasil.stats');
 
+// admin
+ Route::prefix('admin')->group(function () {
+
+    // halaman admin
+    Route::get('/jadwal', [EptScheduleController::class, 'adminIndex'])
+        ->name('admin.jadwal.index');
+    // tambah 
+    Route::post('/jadwal', [EptScheduleController::class, 'store'])
+        ->name('admin.jadwal.store');
+    // update 
+    Route::put('/jadwal/{id}', [EptScheduleController::class, 'update'])
+        ->name('admin.jadwal.update');
+    // hapus 
+    Route::delete('/jadwal/{id}', [EptScheduleController::class, 'destroy'])
+        ->name('admin.jadwal.delete');
+});
 
 
 require __DIR__.'/settings.php';
+
